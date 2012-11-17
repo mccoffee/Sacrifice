5d242077b80ef402819c0c45ebfa94feb6646d4c
Markus <mklimmek@gmx.de>
2012-11-17 21:39:56 +0100

    S-01: Standard Map will be created if a new  Map is constructed

--- src/Sacrifice/Map/Map.java
+++ src/Sacrifice/Map/Map.java
@@ -1,7 +1,32 @@
 package Sacrifice.Map;
 
-public class Map {
-    public Map() {
+import java.util.ArrayList;
 
+public class Map {
+
+    public final int WATER = 0;
+    public final int LAND = 1;
+
+    ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+
+    public Map() {
+        this(32, 32);
+    }
+
+    public Map(int width, int height) {
+        for (int i = 0; i < width; i++) {
+            map.add(new ArrayList<Integer>());
+            for (int j = 0; j < height; j++) {
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+                    map.get(i).add(j, WATER);
+                } else {
+                    map.get(i).add(j, LAND);
+                }
+            }
+        }
+    }
+
+    public int getSquare(int x, int y) {
+        return map.get(x).get(y);
     }
 }
--- test/Sacrifice/Map/MapTest.java
+++ test/Sacrifice/Map/MapTest.java
@@ -1,18 +1,29 @@
 package Sacrifice.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MapTest {
 
-    Map map = new Map();
+    Map map;
 
     @org.junit.Before
     public void setUp() throws Exception {
+        map = new Map();
+    }
+
+    @Test
+    public void testIsWaterAtStandardMap() throws Exception {
+        Assert.assertEquals(0, map.getSquare(0, 0));
 
     }
 
     @Test
-    public void testMapConstruction() throws Exception {
-
+    public void testIsLandAtStandardMap() {
+        Assert.assertEquals(1, map.getSquare(30, 30));
+        Assert.assertEquals(1, map.getSquare(1, 1));
+        Assert.assertEquals(1, map.getSquare(1, 30));
+        Assert.assertEquals(1, map.getSquare(30, 1));
     }
+
 }
